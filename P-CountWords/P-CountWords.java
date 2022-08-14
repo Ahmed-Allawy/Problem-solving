391980647e981baadc0af5566ec3f665d98e1ac0
Ahmed allawy <58919466+Ahmed-Allawy@users.noreply.github.com>
2022-08-14 14:35:17 +0200

    Add files via upload

---
+++ P-CountWords/P-CountWords.java
@@ -0,0 +1,15 @@
+
+//  https://codeforces.com/group/T3p02rhrmb/contest/334358/my
+import java.util.Scanner;
+public class main
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine().trim();
+        String[] arr = s.split("[\\W|\\d|_]+");
+        if(arr[0].length()==0)
+            System.out.println(arr.length-1);
+        else System.out.println(arr.length);
+
+    }
+}
